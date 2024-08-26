717e5b1d26468ee75e7c24f8c4a26e16f2a52e84
EricsonCR <ericson4634@gmail.com>
2024-08-26 07:22:47 -0500

    cambios en: productController, productServices, product_detail.html, dashboardController, properties

--- src/main/java/com/cibertec/netTech/controllers/DashboardController.java
+++ src/main/java/com/cibertec/netTech/controllers/DashboardController.java
@@ -30,52 +30,51 @@ public class DashboardController {
 
 	@Autowired
 	private ProductService productService;
-	
-    @Autowired
-    private HttpSession session; // Asumiendo que tienes acceso a HttpSession
 
-    
+	@Autowired
+	private HttpSession session; // Asumiendo que tienes acceso a HttpSession
+
 	@GetMapping("")
 	public String showDashboard(Model model) {
 		model.addAttribute("title", "Página Dashboard");
-		
+
 		Long userId = (Long) session.getAttribute("userId"); // Asume que el ID del usuario está almacenado en la sesión
-        List<Product> products = productService.findAll();
-        List<Category> categories = categoryService.findAll();
-        model.addAttribute("products", products);
-        model.addAttribute("categories", categories);
-        model.addAttribute("totalProducts", productService.total());
-        model.addAttribute("totalCategories", categoryService.total());
-        model.addAttribute("totalSales", "S/50,000.00");
+		List<Product> products = productService.findAll();
+		List<Category> categories = categoryService.findAll();
+		model.addAttribute("products", products);
+		model.addAttribute("categories", categories);
+		model.addAttribute("totalProducts", productService.total());
+		model.addAttribute("totalCategories", categoryService.total());
+		model.addAttribute("totalSales", "S/50,000.00");
 		return "dashboard";
 	}
-	
+
 	@GetMapping("/products")
 	public String showProducts(Model model) {
 		model.addAttribute("title", "Página Dashboard");
-		
+
 		List<Product> products = productService.findAll();
-        model.addAttribute("products", products);
-        
+		model.addAttribute("products", products);
+
 		return "dashboard";
 	}
-	
+
 	@GetMapping("/products/create")
 	public String showProductsCreate(Model model) {
 		model.addAttribute("title", "Página Form Producto");
-		
+
 		Long userId = (Long) session.getAttribute("userId"); // Asume que el ID del usuario está almacenado en la sesión
-        model.addAttribute("categories", categoryService.findAll());
-        
+		model.addAttribute("categories", categoryService.findAll());
+
 		return "dashboard_products_form";
 	}
-	
+
 	@GetMapping("/categories/create")
 	public String showCategoriesCreate(Model model) {
 		model.addAttribute("title", "Página Form Category");
 		return "dashboard_category_form";
 	}
-	
+
 	@PostMapping("/categories/create")
 	public String categoriesCreate(@RequestParam("name") String name) {
 		Category c = new Category();
@@ -83,29 +82,26 @@ public class DashboardController {
 		categoryService.save(c);
 		return "redirect:/dashboard";
 	}
-	
+
 	@PostMapping("/products/create")
-    public String createProduct(@RequestParam("name") String name,
-                                @RequestParam("price") Integer price,
-                                @RequestParam("image") MultipartFile image,
-                                @RequestParam("category") Long categoryId,
-                                HttpSession session) throws IOException {
-        Long userId = (Long) session.getAttribute("userId");
-        Optional<Category> category = categoryService.findById(categoryId);
-        
-        String imageName = productService.saveImage(image);
-        
-        Product product = new Product();
-        product.setName(name);
-        product.setPrice(price);
-        product.setPhoto(imageName);
-         product.setCategory(category.get());
-        
-        productService.save(product); // Asumiendo que tu servicio puede manejar la carga de imágenes
+	public String createProduct(@RequestParam("name") String name, @RequestParam("price") Integer price,
+			@RequestParam("image") MultipartFile image, @RequestParam("description") String description,
+			@RequestParam("category") Long categoryId, HttpSession session) throws IOException {
+		Long userId = (Long) session.getAttribute("userId");
+		Optional<Category> category = categoryService.findById(categoryId);
 
-        return "redirect:/dashboard";
-    }
-	
-	 
+		String imageName = productService.saveImage(image);
+
+		Product product = new Product();
+		product.setName(name);
+		product.setPrice(price);
+		product.setPhoto(imageName);
+		product.setDescription(description);
+		product.setCategory(category.get());
+
+		productService.save(product); // Asumiendo que tu servicio puede manejar la carga de imágenes
+
+		return "redirect:/dashboard";
+	}
+
 }
-
