60896d271559bc85ca71d203ec716b1d41715939
juanpablocs <juanpablocs21@gmail.com>
2024-08-27 21:16:56 -0500

    fix: Dante index list

--- src/main/java/com/cibertec/netTech/controllers/IndexController.java
+++ src/main/java/com/cibertec/netTech/controllers/IndexController.java
@@ -1,16 +1,34 @@
 package com.cibertec.netTech.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.cibertec.netTech.models.Category;
+import com.cibertec.netTech.models.Product;
+import com.cibertec.netTech.services.CategoryService;
+import com.cibertec.netTech.services.ProductService;
+
 @Controller
 @RequestMapping("")
 public class IndexController {
 
+	@Autowired
+	private CategoryService categoryService;
+
+	@Autowired
+	private ProductService productService;
+	
 	@GetMapping("/")
 	public String showIndex(Model model) {
+		List<Product> products = productService.findAll();
+        List<Category> categories = categoryService.findAll();
+        model.addAttribute("products", products);
+        model.addAttribute("categories", categories);
 		model.addAttribute("title", "Página de Inicio");
 		return "index";
 	}
